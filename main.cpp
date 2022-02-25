1b796cdb73c5e9d7c0b7f140bb9d2811664570ee
ToddPieper1 <98150115+ToddPieper1@users.noreply.github.com>
2022-02-24 21:28:55 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 using namespace std;
 
 int main() {
-    cout << "Hello World!";
-    return 0;
+    Rectangle r1{ 3.0, 5.0 };
+    Square s1{ 4.0 };
+    Circle c1{ 10.0 };
+    // printArea(r1);
+    // printArea(s1);
+    // printArea(c1);
 }
