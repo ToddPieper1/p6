1872f34feaf73e05755bfbc3233a1dc983948d35
ToddPieper1 <98150115+ToddPieper1@users.noreply.github.com>
2022-02-24 22:21:03 -0800

    Add files via upload
    Part 6 responses

--- main.cpp
+++ main.cpp
@@ -20,3 +20,12 @@ int main() {
     printArea(c1);
 }
 
+//Part 6
+// 1) The static polymorphism is the clear winner for ease of implementation, it's simply less lines of code.
+// The issuse with dynamic is the excess need to have to declare the inheritance in each class, and then have to override the function
+// Which we showed to be completely irrelevant with a singular extra line in the main.cpp making a template function.
+// Why bother doing all the extra work in a bunch of side headers and cpp files when you can just do a bit extra in main.
+//
+// 2) I assume this is the case mostly because of what I stated for 1), it's easier and less lines of code. I'm not sure
+// how any difference would affect runtime, if at all, and so to me it has to be a user oriented preference which makes
+// sense considering we scrapped the work for dynamic polymorphism for a seemingly easier static polymorphism experience
